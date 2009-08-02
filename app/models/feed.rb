23a505055fdea877c38a20870eb9d9eb74a29cf9
Matthew Anderson <manderson@onehub.com>
2009-08-01 19:33:18 -0700

    Feeds get auto-generate titles.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,11 +1,12 @@
 class Feed < ActiveRecord::Base
-  
+
   require 'nokogiri'
   require 'open-uri'
-  
+
   validates_presence_of :name, :url
+
   before_validation_on_create :get_feed_info
-  
+
   def get_feed_info
     feed = Nokogiri::XML(open(self.url))
     self.name = feed.css('channel > title').text
