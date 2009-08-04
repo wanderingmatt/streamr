f2ec21f19c6dfe0f32fe92b71f737a87ac8f850d
Matthew Anderson <manderson@onehub.com>
2009-08-03 21:57:58 -0700

    Items are now stored as XML after being salted

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,6 @@
 class Feed < ActiveRecord::Base
 
+  require 'digest/sha1'
   require 'nokogiri'
   require 'open-uri'
 
@@ -8,29 +9,26 @@ class Feed < ActiveRecord::Base
   validates_presence_of :name, :url
 
   before_validation_on_create :set_name
-  
+
   named_scope :active, :conditions => { :active => true }
-  
+
   def set_name
     doc = Nokogiri::XML(open(self.url))
     self.name = doc.css('title').first.text
   end
-  
-    
 
   private
 
   def self.refresh
     feeds = self.active
-    @items = []
 
     feeds.each do |feed|
       parsed_feed = Nokogiri::XML(open(feed.url))
+
       parsed_feed.css('item').each do |item|
-        @items << item
+        hash = Digest::SHA1.hexdigest(item)
+        Item.create(:source => item.to_xml, :salt => hash)
       end
     end
-
-    @items
   end
 end
